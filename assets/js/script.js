c5cfada7341a9bfa059dacb0b34e6c7f49c02ff8
Phil Koch <phil.koch@gmx.ch>
2021-03-22 18:31:23 +0100

    corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,234 +1,93 @@
-function updateDepth(book, newPage) {
-
-	var page = book.turn('page'),
-		pages = book.turn('pages'),
-		depthWidth = 16*Math.min(1, page*2/pages);
-
-		newPage = newPage || page;
-
-	if (newPage>3)
-		$('.sj-book .p2 .depth').css({
-			width: depthWidth,
-			left: 20 - depthWidth
-		});
-	else
-		$('.sj-book .p2 .depth').css({width: 0});
-
-		depthWidth = 16*Math.min(1, (pages-page)*2/pages);
-
-	if (newPage<pages-3)
-		$('.sj-book .p111 .depth').css({
-			width: depthWidth,
-			right: 20 - depthWidth
-		});
-	else
-		$('.sj-book .p111 .depth').css({width: 0});
-
-}
-
-function loadPage(page) {
-
-	$.ajax({url: 'pages/page' + page + '.html'}).
-		done(function(pageHtml) {
-			$('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs/', ''));
-		});
-
-}
+/*
+ * Basic sample
+*/
 
 function addPage(page, book) {
 
 	var id, pages = book.turn('pages');
 
-	if (!book.turn('hasPage', page)) {
+	// Create a new element for this page
+	var element = $('<div />', {});
 
-		var element = $('<div />',
-			{'class': 'own-size',
-				css: {width: 460, height: 582}
-			}).
-			html('<div class="loader"></div>');
+	// Add the page to the flipbook
+	if (book.turn('addPage', element, page)) {
 
-		if (book.turn('addPage', element, page)) {
-			loadPage(page);
-		}
+		// Add the initial HTML
+		// It will contain a loader indicator and a gradient
+		element.html('<div class="gradient"></div><div class="loader"></div>');
 
-	}
-}
-
-function numberOfViews(book) {
-
-	return book.turn('pages') / 2 + 1;
-
-}
-
-function getViewNumber(book, page) {
-
-	return parseInt((page || book.turn('page'))/2 + 1, 10);
-
-}
-
-function zoomHandle(e) {
-
-	if ($('.sj-book').data().zoomIn)
-		zoomOut();
-	else if (e.target && $(e.target).hasClass('zoom-this')) {
-		zoomThis($(e.target));
+		// Load the page
+		loadPage(page, element);
 	}
 
 }
 
-function zoomThis(pic) {
+function loadPage(page, pageElement) {
 
-	var	position, translate,
-		tmpContainer = $('<div />', {'class': 'zoom-pic'}),
-		transitionEnd = $.cssTransitionEnd(),
-		tmpPic = $('<img />'),
-		zCenterX = $('#book-zoom').width()/2,
-		zCenterY = $('#book-zoom').height()/2,
-		bookPos = $('#book-zoom').offset(),
-		picPos = {
-			left: pic.offset().left - bookPos.left,
-			top: pic.offset().top - bookPos.top
-		},
-		completeTransition = function() {
-			$('#book-zoom').unbind(transitionEnd);
+	// Create an image element
 
-			if ($('.sj-book').data().zoomIn) {
-				tmpContainer.appendTo($('body'));
+	var img = $('<img />');
 
-				$('body').css({'overflow': 'hidden'});
-				
-				tmpPic.css({
-					margin: position.top + 'px ' + position.left+'px'
-				}).
-				appendTo(tmpContainer).
-				fadeOut(0).
-				fadeIn(500);
-			}
-		};
-
-		$('.sj-book').data().zoomIn = true;
-
-		$('.sj-book').turn('disable', true);
-
-		$(window).resize(zoomOut);
-		
-		tmpContainer.click(zoomOut);
-
-		tmpPic.load(function() {
-			var realWidth = $(this)[0].width,
-				realHeight = $(this)[0].height,
-				zoomFactor = realWidth/pic.width(),
-				picPosition = {
-					top:  (picPos.top - zCenterY)*zoomFactor + zCenterY + bookPos.top,
-					left: (picPos.left - zCenterX)*zoomFactor + zCenterX + bookPos.left
-				};
-
-
-			position = {
-				top: ($(window).height()-realHeight)/2,
-				left: ($(window).width()-realWidth)/2
-			};
-
-			translate = {
-				top: position.top-picPosition.top,
-				left: position.left-picPosition.left
-			};
-
-			$('.samples .bar').css({visibility: 'hidden'});
-			$('#slider-bar').hide();
-			
-		
-			$('#book-zoom').transform(
-				'translate('+translate.left+'px, '+translate.top+'px)' +
-				'scale('+zoomFactor+', '+zoomFactor+')');
-
-			if (transitionEnd)
-				$('#book-zoom').bind(transitionEnd, completeTransition);
-			else
-				setTimeout(completeTransition, 1000);
-
-		});
-
-		tmpPic.attr('src', pic.attr('src'));
-
-}
-
-function zoomOut() {
-
-	var transitionEnd = $.cssTransitionEnd(),
-		completeTransition = function(e) {
-			$('#book-zoom').unbind(transitionEnd);
-			$('.sj-book').turn('disable', false);
-			$('body').css({'overflow': 'auto'});
-			moveBar(false);
-		};
-
-	$('.sj-book').data().zoomIn = false;
-
-	$(window).unbind('resize', zoomOut);
-
-	moveBar(true);
-
-	$('.zoom-pic').remove();
-	$('#book-zoom').transform('scale(1, 1)');
-	$('.samples .bar').css({visibility: 'visible'});
-	$('#slider-bar').show();
-
-	if (transitionEnd)
-		$('#book-zoom').bind(transitionEnd, completeTransition);
-	else
-		setTimeout(completeTransition, 1000);
-}
-
-
-function moveBar(yes) {
-	if (Modernizr && Modernizr.csstransforms) {
-		$('#slider .ui-slider-handle').css({zIndex: yes ? -1 : 10000});
-	}
-}
-
-function setPreview(view) {
-
-	var previewWidth = 115,
-		previewHeight = 73,
-		previewSrc = 'pages/preview.jpg',
-		preview = $(_thumbPreview.children(':first')),
-		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
-		width = (numPages==1) ? previewWidth/2 : previewWidth;
-
-	_thumbPreview.
-		addClass('no-transition').
-		css({width: width + 15,
-			height: previewHeight + 15,
-			top: -previewHeight - 30,
-			left: ($($('#slider').children(':first')).width() - width - 15)/2
-		});
-
-	preview.css({
-		width: width,
-		height: previewHeight
+	img.mousedown(function(e) {
+		e.preventDefault();
 	});
 
-	if (preview.css('background-image')==='' ||
-		preview.css('background-image')=='none') {
+	img.load(function() {
+		
+		// Set the size
+		$(this).css({width: '100%', height: '100%'});
 
-		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
+		// Add the image to the page after loaded
 
-		setTimeout(function(){
-			_thumbPreview.removeClass('no-transition');
-		}, 0);
+		$(this).appendTo(pageElement);
 
-	}
-
-	preview.css({backgroundPosition:
-		'0px -'+((view-1)*previewHeight)+'px'
+		// Remove the loader indicator
+		
+		pageElement.find('.loader').remove();
 	});
+
+	// Load the page
+
+	img.attr('src', 'pages/' +  page + '.jpg');
+
 }
 
+
+function loadLargePage(page, pageElement) {
+	
+	var img = $('<img />');
+
+	img.load(function() {
+
+		var prevImg = pageElement.find('img');
+		$(this).css({width: '100%', height: '100%'});
+		$(this).appendTo(pageElement);
+		prevImg.remove();
+		
+	});
+
+	// Loadnew page
+	
+	img.attr('src', 'pages/' +  page + '-large.jpg');
+}
+
+
+function loadSmallPage(page, pageElement) {
+	
+	var img = pageElement.find('img');
+
+	img.css({width: '100%', height: '100%'});
+
+	img.unbind('load');
+	// Loadnew page
+
+	img.attr('src', 'pages/' +  page + '.jpg');
+}
+
+
+
+// http://code.google.com/p/chromium/issues/detail?id=128488
 function isChrome() {
-
-	// Chrome's unsolved bug
-	// http://code.google.com/p/chromium/issues/detail?id=128488
 
 	return navigator.userAgent.indexOf('Chrome')!=-1;
 
